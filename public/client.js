40d078f160424adef66167971844441d664b6a83
Tugrul Duyar <Tugrul Duyar>
2016-11-29 15:52:40 +0100

    masterpassword fix

--- public/client.js
+++ public/client.js
@@ -22,7 +22,7 @@ $(document).ready (function() {
    *  */
 
   $('#loginform').submit(function() {
-    socket.emit('login', $('#name').val(), $('#securePassword').val());
+    socket.emit('login', $('#name').val(), $('#password').val());
     $('#name').val('');
     $('#login').hide();
     $('#chat').show();
@@ -33,8 +33,9 @@ $(document).ready (function() {
     /*
     * function to login with the master pw to the chatapp
     */
-   $('securePWform').submit(function() {
-    socket.emit('securePW', $('#password').val());
+   $('#securePWform').submit(function() {
+    socket.emit('securePW', $('#securePassword').val());
+    consol.log("Master Password abgesendet!");
     return false;
   });
     
